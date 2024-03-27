a4b218de7070f51cbd6ab76b11075250f6b8ee7b
jesse <31576321+agentjesse@users.noreply.github.com>
2024-03-27 05:05:42 -0400

    fleshed out html elements, example async API call

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-# webpack-odin-boilerplate
+# odin-weather-app
 ---
-Base boilerplate for new projects. Github repo is also a backup. Created from Odin library project.
-
-the usual dist/distribution folder is named docs for hosting the production code on github pages.
-
--- src/index.js
+++ src/index.js
@@ -1,157 +1,46 @@
 /* Next task:
--  write next tasks for project here, commit messages get buried
-- example task
+- listener for focus out of cityInput, needs to use the city to call the API. need to refactor the async fn
 */
 
 // importing CSS directly into the related js file
 import './styles.css';
 
 // module imports, from named and default
-import { logToConsole as lg, tableToConsole as tb} from "./logger"; //shorthand loggers
-import { functionOne,functionTwo } from './myModule';
-import myName from './myName'; // 'myName' can be changed here, but not in the source
+import { logToConsole as lg, tableToConsole as tb } from './logger'; //shorthand loggers
 
-//modules import testing
-functionOne();
-console.log( functionTwo() );
-console.log( myName('jerry') );
-
-//source map error test
-//cosnole.log( myName('jerry') );
-
-const booksGrid = document.querySelector('.booksGrid');
-const newBookBtn = document.querySelector('#newBookBtn');
-const newBookModal = document.querySelector('#newBookModal');
-const closeModalBtn = document.querySelector('#closeModalBtn');
-const submitBookBtn = document.querySelector('#submitBookBtn');
-const titleInput = document.querySelector('#title');
-const authorInput = document.querySelector('#author');
-const pagesInput = document.querySelector('#pages');
-const readInput = document.querySelector('#read');
-//library that stores book objects will be an array
-const myLibrary = [
-  {
-    title: "The Catcher in the Rye",
-    author: "J.D. Salinger",
-    pages: 277,
-    read: false
-  },
-  {
-    title: "Lord of the Flies",
-    author: "William Golding",
-    pages: 224,
-    read: false
-  },
-  {
-    title: "Fahrenheit 451",
-    author: "Ray Bradbury",
-    pages: 249,
-    read: false
-  },
-  {
-    title: "Animal Farm",
-    author: "George Orwell",
-    pages: 112,
-    read: false
-  },
-  {
-    title: "Brave New World",
-    author: "Aldous Huxley",
-    pages: 311,
-    read: false
+( async ()=>{
+  try {
+    //*free weatherapi api key, replace if compromised from your api account
+    const response = await fetch('https://api.weatherapi.com/v1/current.json?key=XXX4eee2a31767b465e93a211647242603&q=toronto', { mode: 'cors' });
+    if (!response.ok) { throw new Error('Network response was not ok (code 200)'); }
+    // response.json() returns a promise that resolves with the parsed data of the response body.
+    const data = await response.json();
+    lg(data);
+  } catch (error) {
+    lg(error);
   }
-];
 
-//book objects constructor
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages ? pages : 'N/A';
-  this.read = read;
-}
-//Book object methods on the shared prototype object. not using arrow functions for the this value
-Book.prototype.changeReadState = function () {
-  this.read = this.read ? false : true;
-};
+  /*
+  //BELOW IS OLD NON ASYNC/AWAIT CODE:
 
-//create a book, add to library array
-function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push( new Book(title, author, pages, read) );
-}
+  //*free weatherapi api key, replace if compromised from your api account
+  fetch('https://api.weatherapi.com/v1/current.json?key=4eee2a31767b465e93a211647242603&q=toronto', { mode: 'cors' })
+    .then((response) => {
+      // Check if response successful
+      if (!response.ok) { throw new Error('Network response was not ok \(code 200\)') }
+      // response.json() returns a promise to use in another .then
+      // It resolves with the parsed data of the response body.
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      // console.log('Location:', data.location.name);
+      // console.log('Temperature:', data.current.temp_c + '°C');
+      // console.log('Condition:', data.current.condition.text);
+    })
+    .catch((err) => {
+      // Error :(
+    });
+  */
 
-//dummy book objects need to have their prototype chain set:
-myLibrary.forEach( dummyBook=> {
-  Object.setPrototypeOf(dummyBook, Book.prototype);
-  //obj.constructor = Book;// Optionally update constructor reference
-});
-
-//(re)build and display library on page after library array changes ..ouch
-const updateBooksGrid = ()=> {
-  booksGrid.textContent = ''; //clear books before adding
-  myLibrary.forEach( (item,index)=> {
-    //make html elements
-    const bookElem = document.createElement('article');
-    const h3 = document.createElement('h3');
-    const h4 = document.createElement('h4');
-    const pagesPara = document.createElement('p');
-    const readLabel = document.createElement('label');
-    const readInput = document.createElement('input');
-    //wrapper for book removal icon. edit any associated parent child finding logic later
-    const removeWrapper = document.createElement('div');
-    const removeBookIcon = document.createElement('div');
-    //add html elements data
-    h3.textContent = item.title;
-    h4.textContent = item.author;
-    pagesPara.textContent = `Pages: ${item.pages}`;
-    readLabel.textContent = 'Read: ';
-    readInput.setAttribute('type','checkbox');
-    readInput.checked = item.read ? true : false;
-    bookElem.classList.add('book'); //styling class
-    removeWrapper.classList.add('removeWrapper'); //styling class
-    removeBookIcon.setAttribute('data-arr-i',`${index}`);
-    //append to parents
-    readLabel.append(readInput);
-    removeWrapper.append(removeBookIcon);
-    bookElem.append(h3, h4, pagesPara, readLabel, removeWrapper);
-    booksGrid.append(bookElem);
-  });
-};
-updateBooksGrid(); //first run to populate page
-
-//event listeners
-//open modal without bg interactivity, also makes ::backdrop pseudo-element
-newBookBtn.addEventListener('click', e=> {
-  e.stopPropagation();
-  newBookModal.showModal();
-});
-//close and open modal from buttons within a form with method=dialog
-closeModalBtn.addEventListener('click', e=> {
-  e.stopPropagation();
-  newBookModal.close();
-});
-submitBookBtn.addEventListener('click', e=> {
-  e.stopPropagation();
-  //check at least title and author inputs filled
-  if ( titleInput.checkValidity() && authorInput.checkValidity() ) {
-    //use data for new book with the odin required addBookToLibrary fn
-    addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.checked);
-    updateBooksGrid();
-    newBookModal.close(); //also sends a close event, if needed
-  }
-});
-//listener to catch bubbling clicks to remove book from library
-booksGrid.addEventListener('click', e=> {
-  e.stopPropagation();
-  //if book removal div element is clicked
-  if ( e.target.dataset.arrI ) {
-    myLibrary.splice(e.target.dataset.arrI,1);
-    updateBooksGrid();
-  }
-  //if the change read state CHECKBOX was clicked:
-  //event bubbles to booksGrid,just match the checkbox input
-  if (e.target.type === 'checkbox') {
-    myLibrary[
-      +e.target.parentElement.nextElementSibling.children['0'].dataset.arrI
-    ].changeReadState(); //div with data-* attr is next element after
-  }
-});
+} )();
